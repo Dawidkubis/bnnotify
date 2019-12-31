cffba4b5711d03b058f82aedb74a408e60983480
Dawid J. Kubis <dawid.kubis3@gmail.com>
2019-12-31 21:22:19 +0100

    doesn't work

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+extern crate notify_rust;
+
+use notify_rust::Notification;
+use notify_rust::Timeout;
+
+fn main() {
+	Notification::new()
+    	.summary("Firefox News")
+    	.body("This will almost look like a real firefox notification.")
+    	.icon("firefox")
+    	.timeout(Timeout::Milliseconds(6000)) //milliseconds
+    	.show().unwrap();
+}
