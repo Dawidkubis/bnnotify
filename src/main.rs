fe780d7a4f74345205b7632fe492f96c363abeaf
Dawid J. Kubis <dawid.kubis3@gmail.com>
2020-01-30 16:28:10 +0100

    added anyhow; updated code

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,40 @@
 extern crate notify_rust;
+#[macro_use]
+extern crate anyhow;
 
 use notify_rust::Notification;
 use notify_rust::Timeout;
+use anyhow::{Result, Error};
+use std::process::Command;
+use std::str::FromStr;
 
-fn main() {
+struct Acpi {
+	batteries: Vec<Battery>,
+}
+
+struct Battery {
+	id: usize,
+	status: String,
+	percentage: usize,
+	time: String,
+}
+
+impl FromStr for Battery {
+	type Err = Error;
+
+	fn from_str(s: &str) -> Result<Self, Self::Err> {
+		//TODO
+	}
+}
+
+fn main() -> Result<()> {
+
 	Notification::new()
     	.summary("Firefox News")
     	.body("This will almost look like a real firefox notification.")
     	.icon("firefox")
     	.timeout(Timeout::Milliseconds(6000)) //milliseconds
-    	.show().unwrap();
+    	.show()?;
+
+    Ok(())
 }
